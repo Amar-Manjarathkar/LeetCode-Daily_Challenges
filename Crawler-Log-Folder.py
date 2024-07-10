d4505f26b2d726abd26247ead4cd1af493009b9d
Amar Manjarathkar <157588083+Amar-Manjarathkar@users.noreply.github.com>
2024-07-10 23:44:32 +0530

    Create Crawler-Log-Folder.py

---
+++ Crawler-Log-Folder.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minOperations(self, logs):
+        level = 0
+        for log in logs:
+            if log == "../":
+                if level > 0:
+                    level -= 1
+            elif log == "./":
+                continue
+            else:
+                level += 1
+        return level
